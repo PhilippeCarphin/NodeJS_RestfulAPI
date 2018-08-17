6a873fdf8b43d98d4ea50f084a4950d49c001545
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-17 18:54:20 -0400

    Starting request.on('data', function(){...

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 const http = require('http');
 
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 strip_slashes = function(path){
     return path.replace(/^\/+|\/+$/g, '');
@@ -12,22 +13,44 @@ strip_slashes = function(path){
 requestCallback = function(req, res){
 
     // Get the URL and parse it
-    var parsedUrl = url.parse(req.url, true);
+    const parsedUrl = url.parse(req.url, true);
 
 
     // Get the path
-    var trimmedPath = strip_slashes(parsedUrl.pathname);
+    const trimmedPath = strip_slashes(parsedUrl.pathname);
 
 
     // Send the response
     res.end('Hello World\n');
 
 
+    // obtaining the method
+    const method = req.method.toLowerCase();
+
+
+    // obtaining the payload
+    const decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    });
+
+    // get the query string as an object
+    const queryStringObject = parsedUrl.query;
+
+
+
+    req.on('end', function(){
+        console.log('Request : req.on(end)');
+    });
+
     // Logging the request path
     console.log('got request on port 3000 for ' + trimmedPath);
+    console.log('with method ' + method);
+    console.log('and with these query string parameters: ', queryStringObject);
 
 };
-var server = http.createServer(requestCallback);
+const server = http.createServer(requestCallback);
 
 serverUpCallback = function(){
     console.log("the server is listening on port 3000");
