2d027521eb81580a5f3c493663eb9f8986cd7efb
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-17 21:18:44 -0400

    Handler and callback system
    Handlers are expected to take data and a callback.
    
    In the "req.on('end'" part, a handler is chosen based on the path using the
    object router to map paths to handlers.  The handler is called and passed the
    data of the request and a callback to call to terminate the request and send the
    response.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 /*
  * Primary file for the API
  */
-
 const http = require('http');
-
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 
-strip_slashes = function(path){
+
+strip_slashes = function(path)
+{
     return path.replace(/^\/+|\/+$/g, '');
 };
-requestCallback = function(req, res){
 
+const server = http.createServer(function(req, res)
+{
     const parsedUrl = url.parse(req.url, true);
-
-    const path = strip_slashes(parsedUrl.pathname);
+    const path = '/' + strip_slashes(parsedUrl.pathname);
 
     // obtaining the payload
     const decoder = new StringDecoder('utf-8');
@@ -23,24 +23,49 @@ requestCallback = function(req, res){
         buffer += decoder.write(data);
     });
 
-    req.on('end', function(){
+    requestEndCallback = function(){
         buffer += decoder.end();
-        console.log('Request path: ' + path);
-        console.log('and with these query string parameters: ', parsedUrl.query);
-        console.log('with method ' + req.method);
-        console.log('with headers: ', req.headers);
-        console.log('PAYLOAD BUFFER' + buffer);
-        console.log('Request : req.on(end)');
-    });
 
+        const data = {
+            'path' : path,
+            'method' : req.method,
+            'headers' : req.headers,
+            'query' : req.query,
+            'payload': JSON.parse(buffer)
+        };
 
-    // Send the response
-    res.end('Hello World\n');
-};
-const server = http.createServer(requestCallback);
+        requestHandler = (path in router) ? router[path] : handlers.notFound;
 
-serverUpCallback = function(){
+        handlerEndCallback = function(statusCode, payload)
+        {
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+            payload = typeof(payload) == 'object' ? payload : {};
+            res.writeHead(statusCode);
+            res.end(JSON.stringify( payload));
+        };
+        requestHandler(data, handlerEndCallback);
+    };
+    req.on('end', requestEndCallback);
+});
+
+server.listen(3000,function(){
     console.log("the server is listening on port 3000");
+});
+
+var handlers = {};
+
+handlers.root = function(data, callback)
+{
+    console.log('handler for path \'\' called with data:', data);
+    callback(200, {'name' : 'root handler'});
 };
-server.listen(3000,serverUpCallback);
+
+handlers.notFound = function(data, endCallback)
+{
+    endCallback(404, {'path': data.path });
+};
+
+const router = {
+    '/': handlers.root
+};
 
