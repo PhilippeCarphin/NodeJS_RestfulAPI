260a711170b4dc882d4cab79ebcec896e334aa36
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-08-18 22:20:51 -0400

    Refact: Moved handlers to separate file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  */
 const config = require('./config.js');
 const data = require('./lib/data.js');
+const handlers = require('./lib/handlers.js');
 
 const http = require('http');
 const https = require('https');
@@ -90,31 +91,6 @@ var startServers = function()
             "in config", config.envName
         );
     });
-};
-
-var handlers = {};
-/**
- * @param data : The data from the request
- * @param endCallback : function to call when the handling is finished
- */
-handlers.root = function(data, endCallback)
-{
-    console.log('handler for path \'\' called with data:', data);
-    endCallback(200, {'name' : 'root handler'});
-};
-
-handlers.ping = function(data, endCallback)
-{
-    endCallback(200, {});
-};
-
-/**
- * @param data : The data from the request
- * @param endCallback : function to call when the handling is finished
- */
-handlers.notFound = function(data, endCallback)
-{
-    endCallback(404, {'path': data.path });
 };
 
 const router = {
